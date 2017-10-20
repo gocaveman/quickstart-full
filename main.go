581a02335225b81dbc7de7bae28bb10e16d0d619
Brad Peabody <brad@peabody.io>
2017-10-20 04:04:50 -0700

    more basic stuff in there

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"os"
+	"sync"
 
+	"github.com/gocaveman/caveman/filesystem/fsutil"
+	"github.com/gocaveman/caveman/renderer"
 	"github.com/gocaveman/caveman/webutil"
+	"github.com/gocaveman/caveman/webutil/htmlmin"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -16,41 +19,53 @@ var PROGRAM_NAME = "quickstart-full"
 
 func main() {
 
+	pflag.StringP("workdir", "d", "", "Change directory to this value before resolving relative paths")
 	pflag.StringP("http-listen", "l", ":8080", "IP:Port to listen on for HTTP")
+	pflag.StringP("static-dir", "", "static", "Directory for static resources (CSS, JS, images, etc.)")
+	pflag.StringP("views-dir", "", "views", "Directory for view template files (pages)")
+	pflag.StringP("includes-dir", "", "includes", "Directory for include template files")
+	pflag.BoolP("optimize", "o", false, "Enable optimizations such as CSS/JS file combining and minification")
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
+
+	if wd := viper.GetString("workdir"); wd != "" {
+		if err := os.Chdir(wd); err != nil {
+			log.Fatalf("Error changing directory to %q: %v", wd, err)
+		}
+	}
+
+	rend := renderer.New(
+		http.Dir(fsutil.MustAbs(viper.GetString("views-dir"))),
+		http.Dir(fsutil.MustAbs(viper.GetString("includes-dir"))),
+	)
 
 	var hl webutil.HandlerList
 
 	hl = append(hl, webutil.NewContextCancelHandler())
 	hl = append(hl, webutil.NewGzipHandler())
-	hl = append(hl, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if r.URL.Path == "/" {
-			w.Header().Set("content-type", "text/html")
-			fmt.Fprintf(w, "<html><body>Testing!</body></html>")
-			return
-		}
-
-	}))
-	hl = append(hl, http.NotFoundHandler())
-
-	// TODO: I think we can package up the creation of the HTTP server
-	// with good defaults, the log line and the listen into a helper
-	// in webutil - would be cleaner in here - it should probably
-	// start a new goroutine and return if error, so we can start
-	// multiple etc.
-	httpServer := http.Server{
-		Addr:           viper.GetString("http-listen"),
-		Handler:        hl.WithCloseHandler(),
-		ReadTimeout:    30 * time.Second,
-		WriteTimeout:   30 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+	hl = append(hl, webutil.NewCtxSetHandler("main.optimize", viper.GetBool("optimize")))
+	if viper.GetBool("optimize") {
+		hl = append(hl, htmlmin.NewHandler())
 	}
 
-	log.Printf("Listening for HTTP %q", httpServer.Addr)
-	log.Fatal(httpServer.ListenAndServe())
+	hl = append(hl, webutil.NewRequestContextHandler())
 
-	select {} // block forever
+	hl = append(hl, renderer.NewHandler(rend))
+
+	if staticDir := viper.GetString("static-dir"); staticDir != "" {
+		hl = append(hl, webutil.NewStaticFileHandler(http.Dir(fsutil.MustAbs(staticDir))))
+	}
+
+	hl = append(hl, http.NotFoundHandler())
+
+	var wg sync.WaitGroup
+
+	webutil.StartHTTPServer(&http.Server{
+		Addr:    viper.GetString("http-listen"),
+		Handler: hl.WithCloseHandler(),
+	}, &wg)
+
+	wg.Wait()
 
 }
